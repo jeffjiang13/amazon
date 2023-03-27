5bdc61846ea6031a8e77a8aa83cef86d0741e452
Jeff Jiang <jeff.jiang13@gmail.com>
2023-03-26 22:13:54 -0400

    fixing

--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -2,27 +2,10 @@ import { buffer } from "micro";
 import * as admin from "firebase-admin";
 
 // https://console.firebase.google.com/u/1/project/amzn-2/settings/serviceaccounts/adminsdk
-var serviceAccount = require(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
+const serviceAccount = require(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 
-const app = !admin.apps.length
-  ? admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
-    })
-  : admin.app();
-
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
-
-// Find your endpoint's secret in your Dashboard's webhook settings
-// --- OR ---
-// stripe listen --forward-to localhost:3000/api/webhook
-const endpointSecret = process.env.STRIPE_SIGNING_SECRET;
-
-// TEST with command --> `stripe trigger payment_intent.created`
-
-const fulfillOrder = async (session) => {
-  // DEBUG USE: uncomment to see session contents
-  // console.log("Fulfilling order", session);
+const fullFillOrder = async (session) => {
+  console.log("Full fill order", session);
 
   return app
     .firestore()
@@ -32,51 +15,56 @@ const fulfillOrder = async (session) => {
     .doc(session.id)
     .set({
       amount: session.amount_total / 100,
-      amount_shipping: session.total_details.amount_shipping / 100,
+      amount_shipping: session.total_details.amount_shipping / 100 || 30,
       images: JSON.parse(session.metadata.images),
-      timestamp: admin.firestore.FieldValue.serverTimestamp(),
+      timeStamp: admin.firestore.FieldValue.serverTimestamp(),
     })
     .then(() => {
-      console.log(`SUCCESS: Order ${session.id} has been added to the DB`);
+      console.log(`suess: oreder ${session.id} had been added to the DB`);
     })
-    .catch(console.log);
+    .catch((err) => console.log("Erreur a l'insertion !", err.message));
 };
+
+const app = !admin.apps.length
+  ? admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+    })
+  : admin.app();
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+const endpointSecect = process.env.STRIPE_SIGNING_SECRET;
 
 export default async (req, res) => {
   if (req.method === "POST") {
     const requestBuffer = await buffer(req);
-    const payload = requestBuffer.toString();
+    const payloade = requestBuffer.toString();
     const sig = req.headers["stripe-signature"];
 
     let event;
 
-    // Verify that the EVENT posted came from STRIPE
     try {
-      event = stripe.webhooks.constructEvent(payload, sig, endpointSecret);
-    } catch (err) {
-      console.log("ERROR", err.message);
-      return res.status(400).send(`Webhook Error: ${err.message}`);
+      event = stripe.webhooks.constructEvent(payloade, sig, endpointSecect);
+    } catch (error) {
+      console.log("Error", error.message);
+      return res.status(400).send(`webhook error: ${error.message}`);
     }
 
-    // Handle the checkout.session.completed event
+    // handle the chckout session
+
     if (event.type === "checkout.session.completed") {
       const session = event.data.object;
 
-      // Fulfill the purchase... db stuff
-      return fulfillOrder(session)
-        .then(() => {
-          return res.status(200);
-        })
-        .catch((err) => {
-          return res.status(400).send(`Webhook Error: ${err.message}`);
-        });
+      return fullFillOrder(session)
+        .then(() => res.status(200))
+        .catch((err) => res.status(400).send(`Webhook Error: ${err.message}`));
     }
   }
 };
 
 export const config = {
   api: {
-    bodyParser: false, // Disallow body parsing, consume as stream
+    bodyParser: false, // Useless for webhooks
     externalResolver: true,
   },
 };
