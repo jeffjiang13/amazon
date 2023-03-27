9b845e35ef6227d88852ca6a682c6535ed0be93f
Jeff Jiang <jeff.jiang13@gmail.com>
2023-03-26 22:38:22 -0400

    fixing

--- firebase.js
+++ firebase.js
@@ -1,5 +1,5 @@
-import firebase from "firebase/app";
-import "firebase/firestore";
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAyZ5_CEnMfWduTeG19YO9hGgyxFNAulTw",
