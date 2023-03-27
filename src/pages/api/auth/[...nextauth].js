5e6f39e78a61401410ceabba85fa0b5a679cffe3
Jeff Jiang <jeff.jiang13@gmail.com>
2023-03-26 21:35:32 -0400

    fixing

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,13 +1,27 @@
 import NextAuth from "next-auth";
-import Providers from "next-auth/providers";
+import GoogleProvider from "next-auth/providers/google";
 
 export default NextAuth({
-  // Configure one or more authentication providers
   providers: [
-    Providers.Google({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
+
     // ...add more providers here
   ],
+
+  callbacks: {
+    async session({ session, token, user }) {
+      session.user.username = session?.user?.name
+        .split(" ")
+        .join("")
+        .toLocaleLowerCase();
+
+      session.user.uid = token.sub;
+      return session;
+    },
+  },
+
+  secret: process.env.NEXT_PUBLIC_SECRET,
 });
