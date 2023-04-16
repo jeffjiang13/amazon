bd7d6c5edb19ce93ef962cddb0c26f63026040b2
Jeff Jiang <jeff.jiang13@gmail.com>
2023-04-16 00:18:26 -0400

    fixed order date

---
+++ src/components/ProductDetail.js
@@ -0,0 +1,45 @@
+// components/ProductDetail.js
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { addToBasket } from '../slices/basketSlice';
+import Image from 'next/image';
+import styles from './ProductDetail.module.css';
+
+function ProductDetail({ product }) {
+  const dispatch = useDispatch();
+
+  const addItemToBasket = () => {
+    dispatch(
+      addToBasket({
+        ...product,
+        rating,
+        hasPrime,
+      })
+    );
+  };
+
+
+  return (
+    <div className={styles.productDetail}>
+      <div className={styles.productDetail__image}>
+        <Image
+          src={product.image}
+          alt={product.title}
+          width={500}
+          height={500}
+          objectFit="contain"
+        />
+      </div>
+      <div className={styles.productDetail__info}>
+        <h1>{product.title}</h1>
+        <p>{product.description}</p>
+        <h2>${product.price}</h2>
+        <button onClick={addItemToBasket} className={styles.productDetail__addToBasketButton}>
+          Add to Basket
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default ProductDetail;
---
+++ src/pages/[pid].js
@@ -0,0 +1,39 @@
+// pages/[pid].js
+import { useRouter } from 'next/router';
+import { useSelector } from 'react-redux';
+import { selectItems } from '../slices/basketSlice';
+import ProductDetail from '../components/ProductDetail';
+import db from '../firebase';
+
+export async function getServerSideProps(context) {
+  const { pid } = context.query;
+  const product = await db
+    .collection('products')
+    .doc(pid)
+    .get()
+    .then((doc) => {
+      return {
+        id: doc.id,
+        ...doc.data(),
+      };
+    });
+
+  return {
+    props: {
+      product,
+    },
+  };
+}
+
+function ProductPage({ product }) {
+  const router = useRouter();
+
+  return (
+    <div>
+      <button onClick={() => router.back()}>Go back</button>
+      <ProductDetail product={product} />
+    </div>
+  );
+}
+
+export default ProductPage;
