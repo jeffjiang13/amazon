0e6419f220e4147328ccc97baa80e84133ead967
Jeff Jiang <jeff.jiang13@gmail.com>
2023-04-16 19:38:31 -0400

    working on product detail page

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,82 +1,111 @@
-import { StarIcon } from "../../icons";
+import { motion } from "framer-motion";
 import Image from "next/image";
-import { useState } from "react";
+import React, { useState } from "react";
 import Currency from "react-currency-formatter";
+import toast, { Toaster } from "react-hot-toast";
 import { useDispatch } from "react-redux";
+
+import { StarIcon } from "../icons";
 import { addToBasket } from "../slices/basketSlice";
-import Link from "next/link";
-import styles from './Product.module.css';
 
 const MAX_RATING = 5;
 const MIN_RATING = 1;
 
-function Product({ id, title, price, description, category, image }) {
+const Product = ({
+  id,
+  title,
+  price,
+  description,
+  category,
+  image,
+  rating,
+}) => {
   const dispatch = useDispatch();
-
-  const [rating] = useState(
-    Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 1)) + MIN_RATING
+  const [customRating] = useState(
+    Math.floor(Math.random() * (MAX_RATING - MIN_RATING)) + MIN_RATING
   );
   const [hasPrime] = useState(Math.random() < 0.5);
 
-  const addItemToBasket = (e) => {
-    e.stopPropagation();
-    dispatch(
-      addToBasket({
-        id,
-        title,
-        price,
-        rating,
-        description,
-        category,
-        image,
-        hasPrime,
-      })
-    );
+  const addItemTOBasket = () => {
+    const loadingToast = toast.loading("Adding Item...");
+
+    const product = {
+      id,
+      title,
+      price,
+      description,
+      category,
+      image,
+      hasPrime,
+      customRating,
+    };
+
+    dispatch(addToBasket(product));
+
+    toast.success(`Item Added To Basket`, {
+      id: loadingToast,
+
+      position: "bottom-right",
+      style: {
+        textAlign: "center",
+        padding: "18px",
+      },
+    });
   };
 
-// ...
-return (
-  <Link href={`/products/${id}`}>
-    <a>
-      <div className={`${styles.productCard} relative flex flex-col m-5 bg-white z-30 p-10`}>
-        <div className={styles.productContainer}>
-          <p className={`${styles.category} absolute top-2 right-2 text-xs italic text-gray-400`}>
-            {category}
-          </p>
-          <div className="flex flex-col items-start">
-
-          <Image src={image} height={200} width={200} objectFit="contain" />
-          <h4 className={`${styles.title} my-3`}>{title}</h4>
-          <div className={`${styles.rating} flex`}>
-            {Array(rating)
-              .fill()
-              .map((_, i) => (
-                <StarIcon key={i} className="h-5 text-yellow-500" />
-              ))}
-          </div>
-          <p className={`${styles.description} text-xs mt-2 mb-2 line-clamp-2`}>{description}</p>
-          <div className={`${styles.price} mb-5`}>
-            <Currency quantity={price} currency="USD" />
-          </div>
-          {hasPrime && (
-            <div className={`${styles.prime} flex items-center space-x-2 -mt-5`}>
-              <img className="w-12" src="https://links.papareact.com/fdw" alt="" />
-              <p className="text-xs text-gray-500">FREE Next-day Delivery</p>
-            </div>
-          )}
-        </div>
-        </div>
-        <div className={`${styles.buttonContainer} mt-auto`}>
-          <button onClick={addItemToBasket} className="button w-full">
-            Add to Basket
-          </button>
-        </div>
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      viewport={{ once: true }}
+      className="relative flex flex-col m-5 bg-white z-30 p-10 hover:shadow-lg"
+    >
+      <Toaster />
+      <p className="absolute top-2 right-2 text-xs italic text-gray-400">
+        {category}
+      </p>
+      <div className="items-center flex justify-center">
+        <Image
+          src={image}
+          alt={title}
+          height={200}
+          width={200}
+          className="object-contain "
+        />
       </div>
-    </a>
-  </Link>
-);
-// ...
 
-}
+      <h4>{title}</h4>
+      <div className="flex">
+        {Array(customRating)
+          .fill()
+          .map((_, i) => (
+            <StarIcon key={i} className="h-5 text-yellow-500" />
+          ))}
+      </div>
+      <p className="text-xs my-2 line-clamp-2">{description}</p>
+      <div className="mb-5">
+        <Currency quantity={price} currency="USD" />
+      </div>
+      {hasPrime && (
+        <div className="flex items-center space-x-2 -mt-5">
+          <img
+            className="w-12"
+            src="https://www.nicepng.com/png/detail/115-1159983_amazon-prime-logo-prime-amazon.png"
+            alt=""
+          />
+          <p className="text-xs text-gray-500">FREE Next-day Delivery</p>
+        </div>
+      )}
+      <motion.button
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
+        onClick={addItemTOBasket}
+        className="mt-auto button"
+      >
+        Add to Busket
+      </motion.button>
+    </motion.div>
+  );
+};
 
 export default Product;
