f347ddf1cd66276c35d245968b3c2528cb0de7d9
Jeff Jiang <jeff.jiang13@gmail.com>
2023-04-16 00:18:26 -0400

    fixed order date

--- src/components/Product.js
+++ src/components/Product.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 import Currency from "react-currency-formatter";
 import { useDispatch } from "react-redux";
 import { addToBasket } from "../slices/basketSlice";
+import Link from "next/link";
 
 const MAX_RATING = 5;
 const MIN_RATING = 1;
@@ -16,7 +17,8 @@ function Product({ id, title, price, description, category, image }) {
   );
   const [hasPrime] = useState(Math.random() < 0.5);
 
-  const addItemToBasket = () => {
+  const addItemToBasket = (e) => {
+    e.stopPropagation();
     dispatch(
       addToBasket({
         id,
@@ -32,40 +34,37 @@ function Product({ id, title, price, description, category, image }) {
   };
 
   return (
-    <div className="relative flex flex-col m-5 bg-white z-30  p-10 ">
-      <p className="absolute top-2 right-2 text-xs italic text-gray-400">
-        {category}
-      </p>
-
-      <Image src={image} height={200} width={200} objectFit="contain" />
-
-      <h4 className="my-3">{title}</h4>
-
-      <div className="flex">
-        {Array(rating)
-          .fill()
-          .map((_, i) => (
-            <StarIcon key={i} className="h-5 text-yellow-500" />
-          ))}
-      </div>
-
-      <p className="text-xs mt-2 mb-2 line-clamp-2">{description}</p>
-
-      <div className="mb-5">
-        <Currency quantity={price} currency="USD" />
-      </div>
-
-      {hasPrime && (
-        <div className="flex items-center space-x-2 -mt-5">
-          <img className="w-12" src="https://links.papareact.com/fdw" alt="" />
-          <p className="text-xs text-gray-500">FREE Next-day Delivery</p>
+    <Link href={`/products/${id}`}>
+      <a>
+        <div className="relative flex flex-col m-5 bg-white z-30 p-10">
+          <p className="absolute top-2 right-2 text-xs italic text-gray-400">
+            {category}
+          </p>
+          <Image src={image} height={200} width={200} objectFit="contain" />
+          <h4 className="my-3">{title}</h4>
+          <div className="flex">
+            {Array(rating)
+              .fill()
+              .map((_, i) => (
+                <StarIcon key={i} className="h-5 text-yellow-500" />
+              ))}
+          </div>
+          <p className="text-xs mt-2 mb-2 line-clamp-2">{description}</p>
+          <div className="mb-5">
+            <Currency quantity={price} currency="USD" />
+          </div>
+          {hasPrime && (
+            <div className="flex items-center space-x-2 -mt-5">
+              <img className="w-12" src="https://links.papareact.com/fdw" alt="" />
+              <p className="text-xs text-gray-500">FREE Next-day Delivery</p>
+            </div>
+          )}
+          <button onClick={addItemToBasket} className="mt-auto button">
+            Add to Basket
+          </button>
         </div>
-      )}
-
-      <button onClick={addItemToBasket} className="mt-auto button">
-        Add to Basket
-      </button>
-    </div>
+      </a>
+    </Link>
   );
 }
 
