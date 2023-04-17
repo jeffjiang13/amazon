508de2ec929665e37eecd4339992e70b416c981d
Jeff Jiang <jeff.jiang13@gmail.com>
2023-04-17 12:29:45 -0400

    working on product detail page

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -5,24 +5,52 @@ import { addToBasket } from '../slices/basketSlice';
 import Image from 'next/image';
 import styles from './ProductDetail.module.css';
 import Currency from "react-currency-formatter";
-import { FaStar } from 'react-icons/fa';
+import { StarIcon } from "../../icons";
 
-function ProductDetail({ product }) {
+const MAX_RATING = 5;
+const MIN_RATING = 1;
+
+const Product = ({
+  id,
+  title,
+  price,
+  description,
+  category,
+  image,
+  rating,
+}) => {
   const dispatch = useDispatch();
-  const addItemToBasket = () => {
+  const [customRating] = useState(
+    Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 1)) + MIN_RATING
+  );
+  const [hasPrime] = useState(Math.random() < 0.5);
+
+  const addItemTOBasket  = () => {
+    const loadingToast = toast.loading("Adding Item...");
+
     const product = {
       id,
       title,
-      category,
-      description,
-      image,
       price,
-      rating,
+      description,
+      category,
+      image,
+      hasPrime,
+      customRating,
     };
+
     dispatch(addToBasket(product));
+
+    toast.success(`Item Added to Cart`, {
+      id: loadingToast,
+
+      position: "bottom-right",
+      style: {
+        textAlign: "center",
+        padding: "18px",
+      },
+    });
   };
-
-
 
   return (
     <div className="h-screen m-8">
@@ -38,13 +66,13 @@ function ProductDetail({ product }) {
       <div className={styles.productDetail__info}>
       <h4 className="font-bold text-lg mb-4">{product.title}</h4>
         <p>{product.description}</p>
-        <div className="flex items-center mb-2">
-          {Array(Math.round(product.rating.rate))
-            .fill(0)
-            .map((_, i) => (
-              <FaStar key={i} className="text-yellow-500" />
-            ))}
-        </div>
+        <div className="flex mb-3 space-x-1">
+        {Array(customRating)
+          .fill()
+          .map((_, i) => (
+            <StarIcon key={i} className="h-5 text-yellow-500" />
+          ))}
+      </div>
         <div className="mb-3 font-bold">
         <Currency quantity={product.price} currency="USD" />
       </div>
