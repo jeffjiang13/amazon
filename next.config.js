d273ea545f15dd2867d113e31630b3db94ee65d7
Jeff Jiang <jeff.jiang13@gmail.com>
2023-04-17 00:26:44 -0400

    working on product detail page

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 // next.config.js
 module.exports = {
   images: {
-    domains: ["links.papareact.com", "fakestoreapi.com"],
+    domains: ["links.papareact.com", "fakestoreapi.com", "drive.google.com"],
   },
   env: {
     stripe_public_key: process.env.STRIPE_PUBLIC_KEY,
