1105f3f59fee5181fdbc90c2f7cf0b0206271ab4
Jeff Jiang <jeff.jiang13@gmail.com>
2023-04-17 15:29:22 -0400

    working on product detail page

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -20,7 +20,7 @@ function ProductDetail({ product }) {
       image: product.image,
       price: product.price,
       rating: product.rating,
-      hasPrime: Math.random() < 0.5,
+      hasPrime: product.hasPrime,
     };
     dispatch(addToBasket(productToAdd));
     toast.success("Item Added to Cart", {
@@ -43,53 +43,57 @@ function ProductDetail({ product }) {
       }}
       className="relative flex flex-col m-5 bg-white z-30 p-10 shadow-lg"
     >
-      <h4 className="absolute top-2 left-2 font-bold text-lg mb-4">
-        {product.category}
-      </h4>
-      <Toaster />
-      <Image
-        src={product.image}
-        alt={product.title}
-        width={400}
-        height={400}
-        objectFit="contain"
-        className="object-contain"
-      />
-      <div className="col-span-3 mx-5">
-        <h4 className="font-bold text-lg mb-4">{product.title}</h4>
-        <div className="flex">
-          {Array(product.rating)
-            .fill()
-            .map((_, i) => (
-              <StarIcon key={i} className="h-5 text-yellow-500" />
-            ))}
+      <div className="flex items-start">
+        <div className="relative">
+          <h4 className="absolute top-2 left-2 font-bold text-lg mb-4">
+            {product.category}
+          </h4>
+          <Image
+            src={product.image}
+            alt={product.title}
+            width={400}
+            height={400}
+            objectFit="contain"
+            className="object-contain"
+          />
         </div>
-        <p className="text-xs my-2 line-clamp-3">{product.description}</p>
-        <Currency quantity={product.price} currency="USD" />
-        {product.hasPrime && (
-          <div className="flex items-center space-x-2">
-            <img
-              src="https://www.nicepng.com/png/detail/115-1159983_amazon-prime-logo-prime-amazon.png"
-              alt=""
-              loading="lazy"
-              className="w-12"
-            />
-            <p className="text-xs text-gray-500">FREE Next-day Delivery</p>
+        <div className="col-span-3 mx-5">
+          <h4 className="font-bold text-lg mb-4">{product.title}</h4>
+          <div className="flex">
+            {Array(product.rating)
+              .fill()
+              .map((_, i) => (
+                <StarIcon key={i} className="h-5 text-yellow-500" />
+              ))}
           </div>
-        )}
+          <p className="text-xs my-2 line-clamp-3">{product.description}</p>
+          <Currency quantity={product.price} currency="USD" />
+          {product.hasPrime && (
+            <div className="flex items-center space-x-2">
+              <img
+                src="https://www.nicepng.com/png/detail/115-1159983_amazon-prime-logo-prime-amazon.png"
+                alt=""
+                loading="lazy"
+                className="w-12"
+              />
+              <p className="text-xs text-gray-500">FREE Next-day Delivery</p>
+            </div>
+          )}
+        </div>
       </div>
       <div className="flex flex-col space-y-2 my-auto justify-self-end">
         <motion.button
           whileHover={{ scale: 1.02 }}
           whileTap={{ scale: 0.9 }}
           onClick={addItemToBasket}
-          className="button"
+          className="button self-end"
         >
           Add to Cart
         </motion.button>
       </div>
+      <Toaster />
     </motion.div>
- );
+  );
 }
 
 export default ProductDetail;
