09afff86421ed19ff1b465d88d7a12fbff9d8d24
Jeff Jiang <jeff.jiang13@gmail.com>
2023-04-16 23:01:15 -0400

    working on product detail page

--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -3,7 +3,7 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 function ProductFeed({ products }) {
   return (
-    <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-50 mx-auto">
+    <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto">
       {products
         .slice(0, 4)
         .map(({ id, title, price, description, category, image }) => (
