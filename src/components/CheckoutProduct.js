78b90fd78663c80280ddcaa67f44320704c77765
Jeff Jiang <jeff.jiang13@gmail.com>
2023-04-17 00:09:25 -0400

    working on product detail page

--- src/components/CheckoutProduct.js
+++ src/components/CheckoutProduct.js
@@ -1,47 +1,83 @@
-import { useDispatch } from "react-redux";
-import { addToBasket, removeFromBasket } from "../slices/basketSlice";
+import { motion } from "framer-motion";
 import Image from "next/image";
-import { StarIcon } from "../../icons";
+import React from "react";
 import Currency from "react-currency-formatter";
+import toast, { Toaster } from "react-hot-toast";
+import { useDispatch } from "react-redux";
 
-function CheckoutProduct({
+import { addToBasket, removeFromBasket } from "../slices/basketSlice";
+import { StarIcon } from "../icons";
+
+const CheckoutProduct = ({
   id,
   title,
   price,
-  rating,
   description,
   category,
   image,
   hasPrime,
-}) {
+  rating,
+}) => {
   const dispatch = useDispatch();
 
-  const addItemToBasket = () => {
-    dispatch(
-      addToBasket({
-        id,
-        title,
-        price,
-        rating,
-        description,
-        category,
-        image,
-        hasPrime,
-      })
-    );
+  const addItemTOBasket = () => {
+    const loadingToast = toast.loading("Adding Item...");
+
+    const product = {
+      id,
+      title,
+      price,
+      description,
+      category,
+      image,
+      hasPrime,
+      rating,
+    };
+
+    dispatch(addToBasket(product));
+
+    toast.success(`Item Added To Basket`, {
+      id: loadingToast,
+
+      position: "bottom-right",
+      style: {
+        textAlign: "center",
+        padding: "18px",
+      },
+    });
   };
 
   const removeItemFromBasket = () => {
+    const loadingToast = toast.loading("Remove Item...");
+
     dispatch(removeFromBasket({ id }));
+
+    toast.error(`Item Removed from Basket`, {
+      id: loadingToast,
+
+      position: "bottom-right",
+      style: {
+        textAlign: "center",
+        padding: "18px",
+      },
+    });
   };
 
   return (
-    <div className="grid grid-cols-5">
-      <Image src={image} height={200} width={200} objectFit="contain" />
-
-      {/* middle */}
+    <motion.div
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{
+        duration: 0.8,
+        delay: 0.5,
+        ease: [0, 0.71, 0.2, 1.01],
+      }}
+      className="grid grid-cols-5"
+    >
+      <Toaster />
+      <Image src={image} height={200} width={200} className="object-contain" />
       <div className="col-span-3 mx-5">
-        <p>{title}</p>
+      <h4 className="font-bold text-lg mb-4">{title}</h4>
         <div className="flex">
           {Array(rating)
             .fill()
@@ -49,32 +85,40 @@ function CheckoutProduct({
               <StarIcon key={i} className="h-5 text-yellow-500" />
             ))}
         </div>
-        <p className="text-xs mt-2 mb-2 line-clamp-3">{description}</p>
+        <p className="text-xs my-2 line-clamp-3">{description}</p>
         <Currency quantity={price} currency="USD" />
-
         {hasPrime && (
           <div className="flex items-center space-x-2">
             <img
-              className="w-12"
-              src="https://links.papareact.com/fdw"
+              src="https://www.nicepng.com/png/detail/115-1159983_amazon-prime-logo-prime-amazon.png"
               alt=""
+              loading="lazy"
+              className="w-12"
             />
             <p className="text-xs text-gray-500">FREE Next-day Delivery</p>
           </div>
         )}
       </div>
-
-      {/* Right add/remove buttons */}
       <div className="flex flex-col space-y-2 my-auto justify-self-end">
-        <button className="button" onClick={addItemToBasket}>
+        <motion.button
+          whileHover={{ scale: 1.02 }}
+          whileTap={{ scale: 0.9 }}
+          className="button"
+          onClick={addItemTOBasket}
+        >
           Add to Cart
-        </button>
-        <button className="button" onClick={removeItemFromBasket}>
+        </motion.button>
+        <motion.button
+          whileHover={{ scale: 1.02 }}
+          whileTap={{ scale: 0.9 }}
+          onClick={removeItemFromBasket}
+          className="p-2 text-xs md:text-sm bg-gradient-to-b from-yellow-200 to-yellow-400 border border-yellow-300 rounded-sm focus:outline-none focus:ring-2 focus:ring-yellow-500 active:from-yellow-500 hover:bg-gradient-to-b hover:from-orange-200 hover:to-orange-400 hover:border hover:border-orange-300 "
+        >
           Remove from Cart
-        </button>
+        </motion.button>
       </div>
-    </div>
+    </motion.div>
   );
-}
+};
 
 export default CheckoutProduct;
