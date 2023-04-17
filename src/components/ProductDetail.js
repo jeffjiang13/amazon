d061fcbe65f5b51186731a7a3deba60539467aa0
Jeff Jiang <jeff.jiang13@gmail.com>
2023-04-17 13:10:27 -0400

    working on product detail page

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -1,4 +1,3 @@
-// components/ProductDetail.js
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { addToBasket } from '../slices/basketSlice';
@@ -14,19 +13,19 @@ function ProductDetail({ product }) {
   const loadingToast = toast.loading("Adding Item...");
 
   const addItemToBasket = () => {
-    const product = {
-      id,
-      title,
-      category,
-      description,
-      image,
-      price,
-      rating,
-    };
+    // Remove the following line as it redeclares the product variable
+    // const product = {
+    //   id,
+    //   title,
+    //   category,
+    //   description,
+    //   image,
+    //   price,
+    //   rating,
+    // };
     dispatch(addToBasket(product));
     toast.success(`Item Added to Cart`, {
       id: loadingToast,
-
       position: "bottom-right",
       style: {
         textAlign: "center",
@@ -35,49 +34,47 @@ function ProductDetail({ product }) {
     });
   };
 
-
-
   return (
     <motion.div
-    initial={{ opacity: 0 }}
-    whileInView={{ opacity: 1 }}
-    viewport={{ once: true }}
-    className="relative flex flex-col m-5 bg-white z-30 p-10 shadow-lg"
-  >
-
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      viewport={{ once: true }}
+      className="relative flex flex-col m-5 bg-white z-30 p-10 shadow-lg"
+    >
       <Toaster />
-
-        <Image
-          src={product.image}
-          alt={product.title}
-          width={450}
-          height={450}
-          objectFit="contain"
-        />
-
-      <div className={styles.productDetail__info}>
-      <h4 className="font-bold text-lg mb-4">{product.title}</h4>
-        <p>{product.description}</p>
-        <div className="flex mb-3 space-x-1">
-        {Array(Math.round(product.rating.rate))
-          .fill()
-          .map((_, i) => (
-            <StarIcon key={i} className="h-5 text-yellow-500" />
-          ))}
+      <div className="flex">
+        <div>
+          <Image
+            src={product.image}
+            alt={product.title}
+            width={450}
+            height={450}
+            objectFit="contain"
+          />
+        </div>
+        <div className={styles.productDetail__info}>
+          <h4 className="font-bold text-lg mb-4">{product.title}</h4>
+          <p>{product.description}</p>
+          <div className="flex mb-3 space-x-1">
+            {Array(Math.round(product.rating.rate))
+              .fill()
+              .map((_, i) => (
+                <StarIcon key={i} className="h-5 text-yellow-500" />
+              ))}
+          </div>
+          <div className="mb-3 font-bold">
+            <Currency quantity={product.price} currency="USD" />
+          </div>
+          <motion.button
+            whileHover={{ scale: 1.02 }}
+            whileTap={{ scale: 0.9 }}
+            onClick={addItemToBasket}
+            className="mt-auto button"
+          >
+            Add to Cart
+          </motion.button>
+        </div>
       </div>
-        <div className="mb-3 font-bold">
-        <Currency quantity={product.price} currency="USD" />
-      </div>
-      <motion.button
-        whileHover={{ scale: 1.02 }}
-        whileTap={{ scale: 0.9 }}
-        onClick={addItemToBasket}
-        className="mt-auto button"
-      >
-        Add to Cart
-      </motion.button>
-      </div>
-
     </motion.div>
   );
 }
