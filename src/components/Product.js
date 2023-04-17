755d16874897f32c249b0caf1747fc643efaedb2
Jeff Jiang <jeff.jiang13@gmail.com>
2023-04-16 20:10:33 -0400

    working on product detail page

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { FaStar } from "react-icons/fa";
 import { useDispatch } from "react-redux";
-import { Link } from "react-router-dom";
+import Link from "next/link";
 
 import { addToBasket } from "../slices/basketSlice";
 
