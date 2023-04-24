fb80cca4fe42a5dd87d2c248c6ea4b8be267ca4a
Jeff Jiang <jeff.jiang13@gmail.com>
2023-04-24 14:15:12 -0400

    added new features

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 // next.config.js
 module.exports = {
   images: {
-    domains: ["links.papareact.com", "fakestoreapi.com", "drive.google.com"],
+    domains: ["links.papareact.com", "fakestoreapi.com", "drive.google.com", 'api.escuelajs.co'],
   },
   env: {
     stripe_public_key: process.env.STRIPE_PUBLIC_KEY,
