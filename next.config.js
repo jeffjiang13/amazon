0b5be72320b0e88a437034a0d09b7e59d11550aa
Jeff Jiang <jeff.jiang13@gmail.com>
2025-08-05 17:58:26 -0400

    updated readme

--- next.config.js
+++ next.config.js
@@ -2,9 +2,8 @@
 module.exports = {
   images: {
     domains: ["links.papareact.com", "fakestoreapi.com", "drive.google.com"],
-  },
     unoptimized: true,
-
+  },
   env: {
     stripe_public_key: process.env.STRIPE_PUBLIC_KEY,
   },
