a4aca0d59edd6aa5563abddc89142e8d7324239c
Jeff Jiang <jeff.jiang13@gmail.com>
2026-02-24 19:59:11 -0500

    updated readme

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -7,22 +7,15 @@ export default NextAuth({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
     }),
-
-    // ...add more providers here
   ],
-
   callbacks: {
-    async session({ session, token, user }) {
-      console.log('Inside session callback', { session, token, user });
-      session.user.username = session?.user?.name
-        .split(" ")
-        .join("")
-        .toLocaleLowerCase();
-
-      session.user.uid = token.sub;
+    async session({ session, token }) {
+      if (session?.user?.name) {
+        session.user.username = session.user.name.split(" ").join("").toLowerCase();
+      }
+      session.user.uid = token?.sub;
       return session;
     },
   },
-
-  secret: process.env.NEXT_PUBLIC_SECRET,
+  secret: process.env.NEXTAUTH_SECRET,
 });
